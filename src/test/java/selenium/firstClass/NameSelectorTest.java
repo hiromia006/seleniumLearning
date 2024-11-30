b67486560ebb501499312248668d13fa5e889c3b
hiro.mia <hiro.mia@bracits.com>
2024-11-30 09:39:49 +0600

    First class assignment

---
+++ src/test/java/selenium/firstClass/NameSelectorTest.java
@@ -0,0 +1,23 @@
+package selenium.firstClass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class NameSelectorTest {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.name("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.name("login-button")).click();
+
+        driver.quit();
+
+    }
+}
