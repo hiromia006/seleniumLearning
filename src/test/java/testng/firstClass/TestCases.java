29c9435e9dfcf91be1fd74bd8e2838978f2dd51f
hiro.mia <hiro.mia@bracits.com>
2024-12-21 08:00:32 +0600

    TestNG Class

---
+++ src/test/java/testng/firstClass/TestCases.java
@@ -0,0 +1,28 @@
+package testng.firstClass;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCases {
+
+    @Test
+    public void firstTestClass1() {
+        String name = "hello";
+
+        if (name.contains("hello1")) {
+
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+
+        System.out.println("Welcome Test Case");
+    }
+
+    @Test
+    public void firstTestClass2() {
+        String name = "hello";
+        Assert.assertEquals(name, "hello1");
+        System.out.println("Welcome Test Case");
+    }
+}
